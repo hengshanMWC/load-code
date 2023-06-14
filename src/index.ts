7f1a9620419bf1b5fe6c66c7a8ee8c553749231d
马文灿 <mawencan@cvte.com>
2023-06-14 17:15:21 +0800

    feat: 提供loadCjsCode，隐藏loadEsmCode

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { mkdtemp, readFile, unlink, writeFile } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 import { extname } from 'node:path'
 import type { TransformOptions } from 'esbuild'
 import { transform } from 'esbuild'
 
 export function requireCjsLoad(code: string) {
-  const fn = new Function('require', 'exports', 'module', code)
+  const fn = new Function('exports', 'require', 'module', '__filename', '__dirname', code)
   const mod = { exports: {} }
   const moduleContext = { exports: mod.exports, module: mod }
-  fn(require, mod.exports, moduleContext.module)
-  return mod.exports
+  return () => {
+    fn(mod.exports, require, moduleContext.module, __filename, __dirname)
+    return mod.exports
+  }
 }
 export async function loadCjsCode(file: string, loader?: any) {
   const str = await readFile(file, 'utf8')
@@ -30,34 +32,34 @@ function getLoaderMap(file: string): TransformOptions['loader'] {
   return loaderMap[ext] || 'js'
 }
 
-export async function loadEsmCode(file: string, loader?: any) {
-  const str = await readFile(file, 'utf8')
-  const res = await transform(str, {
-    loader: loader || getLoaderMap(file),
-    format: 'esm',
-  })
-  return requireEsmLoad(res.code)
-}
+// export async function loadEsmCode(file: string, loader?: any) {
+//   const str = await readFile(file, 'utf8')
+//   const res = await transform(str, {
+//     loader: loader || getLoaderMap(file),
+//     format: 'esm',
+//   })
+//   return requireEsmLoad(res.code)
+// }
 
-export async function requireEsmLoad(code: string) {
-  let tempDir
-  try {
-    tempDir = await mkdtemp(process.env.TMPDIR || 'temp-')
-    console.log(tempDir)
-  }
-  catch (err) {
-    console.log(err)
-  }
-  const filePath = `${tempDir}/temp-file.txt`
-  console.log(filePath)
-  await writeFile(filePath, code, 'utf8')
-  return async () => {
-    try {
-      const data = await import(filePath)
-      return data
-    }
-    finally {
-      unlink(filePath)
-    }
-  }
-}
+// export async function requireEsmLoad(code: string) {
+//   let tempDir
+//   try {
+//     tempDir = await mkdtemp(process.env.TMPDIR || 'temp-')
+//     console.log(tempDir)
+//   }
+//   catch (err) {
+//     console.log(err)
+//   }
+//   const filePath = `${tempDir}/temp-file.txt`
+//   console.log(filePath)
+//   await writeFile(filePath, code, 'utf8')
+//   return async () => {
+//     try {
+//       const data = await import(filePath)
+//       return data
+//     }
+//     finally {
+//       unlink(filePath)
+//     }
+//   }
+// }
--- test/specs/index.test.ts
+++ test/specs/index.test.ts
@@ -1,20 +1,21 @@
 import { join } from 'node:path'
 import { describe, expect, test } from 'vitest'
 import type { TransformOptions } from 'esbuild'
-import { loadCjsCode, loadEsmCode } from '../../src/index'
+import { loadCjsCode } from '../../src/index'
 import testData from '../__fixtures__/file/test'
 
 async function handleTest(ext: string, loader?: TransformOptions['loader']) {
-  const jsData = await loadCjsCode(join(__dirname, `../__fixtures__/file/test.${ext}`), loader)
+  const fn = await loadCjsCode(join(__dirname, `../__fixtures__/file/test.${ext}`), loader)
+  const jsData = await fn()
   expect(getExportDefault(jsData)).toEqual(testData)
 }
 
-async function handleTest2(ext: string, loader?: TransformOptions['loader']) {
-  const fn = await loadEsmCode(join(__dirname, `../__fixtures__/file/test.${ext}`), loader)
-  const jsData = await fn()
-  console.log(jsData)
-  expect(getExportDefault(jsData)).toEqual(testData)
-}
+// async function handleTest2(ext: string, loader?: TransformOptions['loader']) {
+//   const fn = await loadEsmCode(join(__dirname, `../__fixtures__/file/test.${ext}`), loader)
+//   const jsData = await fn()
+//   console.log(jsData)
+//   expect(getExportDefault(jsData)).toEqual(testData)
+// }
 export function getExportDefault(code: any) {
   return code?.__esModule ? code.default : code
 }
@@ -26,11 +27,11 @@ describe('test', () => {
     arr.push(handleTest('cjs', 'js'))
     await arr
   })
-  test('loadEsmCode', async () => {
-    const arr = ['ts', 'js', 'mjs'].map((key) => {
-      return handleTest2(key)
-    })
-    arr.push(handleTest2('cjs', 'js'))
-    await arr
-  })
+  // test('loadEsmCode', async () => {
+  //   const arr = ['ts', 'js', 'mjs'].map((key) => {
+  //     return handleTest2(key)
+  //   })
+  //   arr.push(handleTest2('cjs', 'js'))
+  //   await arr
+  // })
 })
