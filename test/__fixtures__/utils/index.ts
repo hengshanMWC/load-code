9ccde0abcca4350e5d925970dbb7433b2a608724
马文灿 <mawencan@cvte.com>
2023-06-27 14:40:45 +0800

    chore: 删除无用的文件

--- test/__fixtures__/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { mkdtemp } from 'node:fs/promises'
-import { join } from 'node:path'
-
-export async function similarDirectory(prefix: string, dir?: string) {
-  const path = await mkdtemp(`${process.env.TMPDIR || '/.tmp/code-'}${prefix}-`)
-  return dir ? join(path, dir) : path
-}
