0eee26391db0e0cac591f4e921c8f670ae9e3489
mawencan <1819293933@qq.com>
2023-09-07 20:06:25 +0800

    test: 补充loadFileCode测试

--- test/specs/index.test.ts
+++ test/specs/index.test.ts
@@ -1,16 +1,23 @@
 import { join } from 'node:path'
 import { describe, expect, test } from 'vitest'
-import { loadConfig } from '../../src/index'
+import { loadConfig, loadFileCode } from '../../src/index'
 
 const id = 'test'
 const config = {
   id,
 }
+const dir = '../__fixtures__/file'
 describe('test', () => {
-  test('base', async () => {
-    const dirPath = join(__dirname, '../__fixtures__/file')
-    const { data, path } = await loadConfig(id, join(__dirname, '../__fixtures__/file'))
+  test('loadConfig', async () => {
+    const dirPath = join(__dirname, dir)
+    const { data, path } = await loadConfig(id, join(__dirname, dir))
     expect(data).toEqual(config)
     expect(path).toBe(join(dirPath, (`${id}.config.ts`)))
+  })
+  test('loadFileCode', async () => {
+    const dirPath = join(__dirname, dir)
+    const { data, path } = await loadFileCode(join(__dirname, dir, 'test.config.json'))
+    expect(data).toEqual(config)
+    expect(path).toBe(join(dirPath, (`${id}.config.json`)))
   })
 })
